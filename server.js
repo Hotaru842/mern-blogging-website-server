de5e924f7a41a3e1d44c5718d487cbb77459b048
UnicornDad8 <shiningforce972@gmail.com>
2025-06-17 17:49:16 -0300

    Agregar la ruta '/add-comment'

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ import aws from "aws-sdk";
 import User from "./Schema/User.js";
 import Blog from "./Schema/Blog.js";
 import Notification from "./Schema/Notification.js";
+import Comment from "./Schema/Comment.js";
 
 const server = express();
 let PORT = 3000;
@@ -442,6 +443,14 @@ server.post("/like-blog", verifyJWT, (req, res) => {
       like.save().then(notification => {
         return res.status(200).json({ liked_by_user: true })
       })
+    } else {
+      Notification.findOneAndDelete({ user: user_id, blog: _id, type: "like" })
+      .then(data => {
+        return res.status(200).json({ liked_by_user: false });
+      })
+      .catch(err => {
+        return res.status(500).json({ error: err.message });
+      })
     }
   })
 });
@@ -458,7 +467,53 @@ server.post("/is-liked-by-user", verifyJWT, (req, res) => {
   .catch(err => {
     return res.status(500).json({ error: err.message });
   })
-})
+});
+
+server.post("/add-comment", verifyJWT, (req, res) => {
+  let user_id = req.user;
+  let { _id, comment, blog_author } = req.body;
+
+  if(!comment?.length) {
+    return res.status(403).json({ error: "You must write something to leave a comment"})
+  }
+
+  let commentObj = new Comment({
+    blog_id: _id,
+    blog_author,
+    comment,
+    commented_by: user_id,
+  })
+
+  commentObj.save().then(commentFile => {
+    let { comment, commentedAt, children } = commentFile;
+
+    Blog.findOneAndUpdate({ _id }, { $push: { "comments": commentFile._id }, 
+    $inc: { "activity.total_comments": 1 }, "activity.total_parent_comments": 1})
+    .then(blog => {
+      console.log("New comment created");
+    })
+
+    let notificationObj = {
+      type: "comment",
+      blog: _id,
+      notification_for: blog_author,
+      user: user_id,
+      comment: commentFile._id
+    }
+
+    new Notification(notificationObj).save().then(notification => {
+      console.log("New notification created");
+    })
+
+    return res.status(200).json({
+      comment,
+      commentedAt,
+      _id: commentFile._id,
+      user_id,
+      children 
+    })
+  })
+});
 
 server.listen(PORT, () => {
   console.log("listening on port => " + PORT);
