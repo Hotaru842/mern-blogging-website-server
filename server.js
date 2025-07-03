393d034176f76dc2fc5ea4060aae218e785691c7
UnicornDad8 <shiningforce972@gmail.com>
2025-07-03 19:20:42 -0300

    Agregar ruta llamada '/user-written-blogs-count'

--- server.js
+++ server.js
@@ -831,6 +831,20 @@ server.post("/user-written-blogs", verifyJWT, (req, res) => {
   });
 });
 
+server.post("/user-written-blogs-count", verifyJWT, (req, res) => {
+  let user_id = req.user;
+  let { draft, query } = req.body;
+
+  Blog.countDocuments({ author: user_id, draft, title: new RegExp(query, "i")})
+  .then(count => {
+    return res.status(200).json({ totalDocs: count });
+  })
+  .catch(err => {
+    console.log(err.message);
+    return res.status(500).json({ error: err.message });
+  })
+});
+
 server.listen(PORT, () => {
   console.log("listening on port => " + PORT);
 });
